0c9d6e72b1d0600a907615f50541d44ef8ea92c5
Christopher Espiritu <christopheredrian@gmail.com>
2017-12-28 01:34:18 +0800

    Refactored code

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
 const express = require("express");
-const passport = require("passport");
-const keys = require("./config/keys");
+require('./services/passport');
 
-const GoogleStrategy = require("passport-google-oauth20");
 const app = express();
 
-// Use the GoogleStrategy in passport
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken);
-      console.log(refreshToken);
-      console.log(profile);
-    }
-  )
-);
+require('./routes/authRoutes')(app);
 
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
-  })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
 
 app.get("/", (req, res) => {
   res.send({ name: "cee!" });
